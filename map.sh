a5a83aba9a9e1a20065fad905e6d11086c1da042
loksin008 <79530873+loksin008@users.noreply.github.com>
2024-11-04 13:40:53 +0530

    Create map.sh

---
+++ map.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Display ASCII map of India
+echo "Map of India"
+echo "------------"
+echo "
+       ,----,                 
+   ,/   .`|                 
+ ,`   .'  :   ,---,         
