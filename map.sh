2b57b880a45c195bdabca58d58dcf129ae8288ff
loksin008 <79530873+loksin008@users.noreply.github.com>
2024-11-04 13:48:30 +0530

    Update map.sh

--- map.sh
+++ map.sh
@@ -4,6 +4,16 @@
 echo "Map of India"
 echo "------------"
 echo "
-       ,----,                 
-   ,/   .`|                 
- ,`   .'  :   ,---,         
+        ______
+       |  _  |
+       | (_) |
+    __ |  _  | __
+   |  || | | ||  |
+   |__||_|_|_||__|
+        /_\\
+      _/   \\_
+     |_______|
+"
+
+echo ""
+echo "Note: This is a very simplified ASCII representation of India's map."
